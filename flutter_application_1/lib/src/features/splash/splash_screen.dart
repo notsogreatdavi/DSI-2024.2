ac27cd3232e61204ac4ad9fb7f18dea13113cf12
Guilherme <guilherme.leo2012@gmail.com>
2024-12-10 22:31:15 -0300

    Adicionado SplahsPage, fonte padrão, imagem

---
+++ flutter_application_1/lib/src/features/splash/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../../common/constants/app_colors.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              AppColors.azulEscuro,
+              AppColors.azulEscuro,
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisSize:
+              MainAxisSize.min, // Ajusta o tamanho para centralizar os filhos
+          children: [
+            Image.asset(
+              'assets/images/RatoBrancoFundoAzul.png', // Substitua pelo caminho correto da sua imagem
+              width: 220, // Ajuste o tamanho conforme necessário
+              height: 220,
+            ),
+            const SizedBox(height: 10), // Espaçamento entre a imagem e o texto
+            const Text(
+              "Mind Rats",
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFF0F0F0),
+                fontFamily: 'Modak',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
