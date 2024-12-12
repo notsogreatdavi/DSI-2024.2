87ccad9f28c9d529bd6c0068754455cbad188575
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2024-12-12 19:48:03 -0300

    Criei o Navigator para as telas atuais
    Criei o navigator para as telas atuais e criei um modelo simples para começar a implementação da tela de ranking

---
+++ flutter_application_1/lib/src/features/ranking/ranking_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../../common/constants/app_colors.dart';
+
+class RankingScreen extends StatelessWidget {
+  const RankingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.azulEscuro,
+            foregroundColor: AppColors.azulClaro1,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          ),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Botão clicado!')),
+            );
+          },
+          child: const Text('Clique aqui'),
+        ),
+      ),
+    );
+  }
+}
