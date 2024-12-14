151742fa33268e2efd7b7e6b8026c83f3a7802e2
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2024-12-14 16:49:05 -0300

    Ajustei o roteamento

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -4,19 +4,23 @@ import 'src/features/onboarding/onboarding_screen.dart';
 import 'src/features/ranking/ranking_screen.dart';
 import 'src/features/login/login_screen.dart';
 import 'src/features/cadastro/cadastro_screen.dart';
+import 'src/features/home/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage(),
-      initialRoute: '/tela_cadastro',
+    return MaterialApp(
+      initialRoute: '/',
       routes: {
-        '/tela_login': (context) => LoginPage(),
-        '/tela_ranking': (context) => RankingScreen(key: GlobalKey()),
-        '/tela_cadastro': (context) => CadastroPage(),
-        }
-       );
+        '/': (context) => const SplashScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => LoginPage(),
+        '/cadastro': (context) => CadastroPage(),
+        '/home': (context) => const HomeScreen(),
+        '/ranking': (context) => const RankingScreen(),
       }
+    );
   }
+}
