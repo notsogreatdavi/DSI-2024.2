12366de7190bde2d29aff7915c613b79e3688d79
Davi Vieira <davivdsilva03@gmail.com>
2024-12-13 22:47:57 -0300

    criação tela de cadastro e login

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'src/features/splash/splash_screen.dart';
 import 'src/features/onboarding/onboarding_screen.dart';
 import 'src/features/ranking/ranking_screen.dart';
+import 'src/features/login/login_screen.dart';
+import 'src/features/cadastro/cadastro_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
+    return MaterialApp(home: LoginPage(),
+      initialRoute: '/tela_cadastro',
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/onboarding': (context) => const OnboardingScreen(),
-        '/ranking': (context) => const RankingScreen(),
+        '/tela_login': (context) => LoginPage(),
+        '/tela_ranking': (context) => RankingScreen(key: GlobalKey()),
+        '/tela_cadastro': (context) => CadastroPage(),
+        }
+       );
       }
-    );
   }
-}
