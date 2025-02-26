db455aaf67f0158c64c702d30ae7668ce7d494f5
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-02-26 04:03:51 -0300

    Criada a tela de esqueci-a-senha

--- flutter_application_1/lib/src/features/login/forgot_password.dart
+++ flutter_application_1/lib/src/features/login/forgot_password.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_application_1/src/common/constants/app_colors.dart';
-import 'package:uni_links/uni_links.dart'; 
+import 'package:app_links/app_links.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:shared_preferences/shared_preferences.dart'; 
 import 'dart:async';
 
 class ForgotPasswordPage extends StatefulWidget {
@@ -18,120 +17,14 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final TextEditingController _newPasswordController = TextEditingController();
   final TextEditingController _confirmPasswordController = TextEditingController();
   final SupabaseClient _supabase = Supabase.instance.client;
-  StreamSubscription? _sub;
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _linkSubscription;
 
   bool _isResetPassword = false;
+  String? _resetCode;
+  String? _resetEmail;
 
-  @override
-  void initState() {
-    super.initState();
-    _initDeepLinkListener();
-    _checkResetPasswordState();
-  }
-
-  @override
-  void dispose() {
-    _sub?.cancel();
-    super.dispose();
-  }
-
-  void _initDeepLinkListener() {
-    if (kIsWeb) {
-      // Verificar parâmetros da URL na web
-      final uri = Uri.base;
-      print('URI atual: $uri'); // Verifique a URI no console
-      if (uri.queryParameters.containsKey('reset-password')) {
-        _setResetPasswordState(true);
-      }
-    } else {
-      // Usar uni_links para mobile
-      _sub = uriLinkStream.listen((Uri? uri) {
-        if (uri != null && uri.host == 'reset-password') {
-          _setResetPasswordState(true);
-        }
-      }, onError: (err) {
-        _showMessage('Erro ao processar o deep link: $err');
-      });
-    }
-  }
-
-  Future<void> _checkResetPasswordState() async {
-    final prefs = await SharedPreferences.getInstance();
-    final isResetPassword = prefs.getBool('isResetPassword') ?? false;
-    if (isResetPassword) {
-      setState(() {
-        _isResetPassword = true;
-      });
-    }
-  }
-
-  Future<void> _setResetPasswordState(bool value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isResetPassword', value);
-    setState(() {
-      _isResetPassword = value;
-    });
-  }
-
-  Future<void> _resetPassword() async {
-    final email = _emailController.text.trim();
-
-    if (email.isEmpty) {
-      _showMessage('Por favor, insira seu email.');
-      return;
-    }
-
-    try {
-      // Obter a porta dinâmica usada pelo Flutter
-      final uri = Uri.base;
-      final port = uri.port; // Porta atual (ex: 52050)
-
-      final redirectTo = kIsWeb
-          ? 'http://localhost:$port/reset-password?reset-password=true' // URL para web (localhost)
-          : 'meuapp://reset-password'; // URL para mobile
-
-      // Enviar email de redefinição de senha
-      await _supabase.auth.resetPasswordForEmail(
-        email,
-        redirectTo: redirectTo,
-      );
-
-      _showMessage('Um link para redefinir sua senha foi enviado para o seu email.');
-    } catch (error) {
-      if (error.toString().contains('rate limit exceeded')) {
-        _showErrorDialog('Aguarde alguns minutos antes de solicitar um novo email.');
-      } else {
-        _showMessage('Erro ao solicitar redefinição de senha. Tente novamente mais tarde.');
-      }
-    }
-  }
-
-  Future<void> _updatePassword() async {
-    final newPassword = _newPasswordController.text.trim();
-    final confirmPassword = _confirmPasswordController.text.trim();
-
-    if (newPassword.isEmpty || confirmPassword.isEmpty) {
-      _showMessage('Por favor, preencha todos os campos.');
-      return;
-    }
-
-    if (newPassword != confirmPassword) {
-      _showMessage('As senhas não coincidem.');
-      return;
-    }
-
-    try {
-      await _supabase.auth.updateUser(
-        UserAttributes(password: newPassword),
-      );
-      _showMessage('Senha atualizada com sucesso.');
-      await _setResetPasswordState(false); // Resetar o estado
-      Navigator.pop(context);
-    } catch (error) {
-      _showMessage('Erro ao atualizar a senha. Tente novamente mais tarde.');
-    }
-  }
-
+  // Métodos de exibição de mensagens
   void _showMessage(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -178,13 +71,149 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
               ),
             ),
           ],
+          backgroundColor: AppColors.branco,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
-          backgroundColor: AppColors.branco,
         );
       },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinkListener();
+    _checkUrlForResetCode(); // Verifica a URL ao iniciar
+  }
+
+  void _initDeepLinkListener() async {
+    _appLinks = AppLinks();
+
+    // Verificar deep link inicial
+    final initialLink = await _appLinks.getInitialLink();
+    if (initialLink != null) {
+      _handleDeepLink(initialLink);
+    }
+
+    // Escutar deep links subsequentes
+    _linkSubscription = _appLinks.uriLinkStream.listen((Uri uri) {
+      _handleDeepLink(uri);
+    });
+  }
+
+  void _handleDeepLink(Uri uri) {
+    if (uri.host == 'reset-password' || uri.path == '/reset-password') {
+      final code = uri.queryParameters['code'];
+      final email = uri.queryParameters['email']; // Recuperar o email da URL
+      if (code != null && email != null) {
+        setState(() {
+          _isResetPassword = true;
+          _resetCode = code;
+          _resetEmail = email;
+          _emailController.text = email; // Preencher o campo de email
+        });
+      }
+    }
+  }
+
+  void _checkUrlForResetCode() async {
+    if (kIsWeb) {
+      final uri = Uri.base; // Obtém a URL atual
+      final code = uri.queryParameters['code']; // Verifica se há um parâmetro 'code'
+      final email = uri.queryParameters['email']; // Verifica se há um parâmetro 'email'
+      if (code != null && email != null) {
+        setState(() {
+          _isResetPassword = true;
+          _resetCode = code;
+          _resetEmail = email;
+          _emailController.text = email; // Preencher o campo de email
+        });
+      }
+    }
+  }
+
+  Future<void> _resetPassword() async {
+    final email = _emailController.text.trim();
+
+    if (email.isEmpty) {
+      _showMessage('Por favor, insira seu email.');
+      return;
+    }
+
+    try {
+      final uri = Uri.base;
+      final port = uri.port;
+
+      final redirectTo = kIsWeb
+          ? 'http://localhost:$port/reset-password?code=$_resetCode&email=$email' // URL para web
+          : 'meuapp://reset-password?code=$_resetCode&email=$email'; // URL para mobile
+
+      print('URL de redirecionamento: $redirectTo'); // Log da URL
+
+      await _supabase.auth.resetPasswordForEmail(
+        email,
+        redirectTo: redirectTo,
+      );
+
+      _showMessage('Um link para redefinir sua senha foi enviado para o seu email.');
+    } catch (error) {
+      print('Erro ao solicitar redefinição de senha: $error'); // Log do erro
+      if (error.toString().contains('rate limit exceeded')) {
+        _showErrorDialog('Aguarde alguns minutos antes de solicitar um novo email.');
+      } else {
+        _showMessage('Erro ao solicitar redefinição de senha. Tente novamente mais tarde.');
+      }
+    }
+  }
+
+Future<void> _updatePassword() async {
+  final newPassword = _newPasswordController.text.trim();
+  final confirmPassword = _confirmPasswordController.text.trim();
+
+  if (newPassword.isEmpty || confirmPassword.isEmpty) {
+    _showMessage('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  if (newPassword != confirmPassword) {
+    _showMessage('As senhas não coincidem.');
+    return;
+  }
+
+  try {
+    if (_resetCode != null && _resetEmail != null) {
+      print('Código de redefinição: $_resetCode'); // Log do código de redefinição
+      print('Email de redefinição: $_resetEmail'); // Log do email
+
+      await _supabase.auth.updateUser(
+        UserAttributes(password: newPassword),
+      );
+      _showMessage('Senha atualizada com sucesso.');
+      setState(() {
+        _isResetPassword = false;
+        _resetCode = null;
+        _resetEmail = null;
+      });
+      if (kIsWeb) {
+        // Redirecionar para a página inicial ou login
+        Navigator.pushReplacementNamed(context, '/login');
+      } else {
+        Navigator.pop(context);
+      }
+    } else {
+      _showMessage('Código de redefinição ou email inválido.');
+    }
+  } catch (error) {
+    print('Erro ao atualizar a senha: $error'); // Log do erro
+    _showMessage('Erro ao atualizar a senha. Tente novamente mais tarde.');
+  }
+}
+
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+    super.dispose();
   }
 
   @override
